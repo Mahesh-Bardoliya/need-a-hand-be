f77afbd0247871bd667ffea7d98543c16221ef59
Mahesh-Bardoliya <166529909+Mahesh-Bardoliya@users.noreply.github.com>
2025-05-04 10:01:54 +0530

    quick fix

--- need_a_hand_be/routes/auth.py
+++ need_a_hand_be/routes/auth.py
@@ -89,7 +89,7 @@ def login(
         {"email": db_user.email, "username": db_user.username}
     )
     response.set_cookie(
-        key="access_token", value=f"Bearer {access_token}", httponly=True
+        key="access_token", value=f"Bearer {access_token}", httponly=True, samesite="none"
     )
 
 
