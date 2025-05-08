a0d458878e5bd1491701577a5feebd02112bd958
Mahesh Bardoliya <mpbardoliya@gmail.com>
2025-05-08 08:52:22 +0530

    Restrict CORS origins to specific URLs

--- need_a_hand_be/config.py
+++ need_a_hand_be/config.py
@@ -16,7 +16,11 @@ class Settings(BaseSettings):
     # NOTE: Currently by default, we are allowing all origins.
     # In future we need to provide only specific ones.
     # E.g. frontend origin URL.
-    cors_allow_origins: typing.List[str] = ["*"]
+    cors_allow_origins: typing.List[str] = [
+        "https://preview--hand-up-community-app.lovable.app",
+        "https://localhost",
+        "capacitor://localhost",
+    ]
 
     # JWT algorithm.
     jwt_algorithm: str = "HS256"
